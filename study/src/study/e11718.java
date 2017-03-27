50cc52652b4b04438278d2ba8a174c5c91841546
Kim Yeonsu <Kim Yeonsu@192.168.1.66>
2017-03-27 10:05:54 +0900

    ee

--- study/src/study/e11718.java
+++ study/src/study/e11718.java
@@ -4,11 +4,11 @@ package study;
 import java.util.*;
 
 public class e11718 {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         String str;
         while((str = sc.nextLine()) != null){
             System.out.println(str);
         }
-	}
+    }
 }
